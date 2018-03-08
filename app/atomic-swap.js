700cab2444652c8e062a1f59ab803a2e514cbddd
Antonina Norair <antonina.norair@gmail.com>
2018-03-08 14:50:03 -0800

    More changes to atomic swap nodejs code

--- app/atomic-swap.js
+++ app/atomic-swap.js
@@ -1,28 +1,42 @@
 var contract = require('truffle-contract')
 var Web3 = require('web3');
-
 var config = require('./config.js')
 
 var web3 = new Web3(new Web3.providers.HttpProvider(config.blockchainNodeAdrress));
 //console.log("blockchain address = " + config.blockchainNodeAddress);
 
-var HTLContract = contract(require('../build/contracts/HashedTimelock.json'))
-HTLContract.setProvider(web3.currentProvider)
-
+var HTLC= contract(require('../build/contracts/HashedTimelock.json'))
+HTLC.setProvider(web3.currentProvider)
 //dirty hack for web3@1.0.0 support for localhost testrpc, see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
-if (typeof HTLContract.currentProvider.sendAsync !== "function") {
-  HTLContract.currentProvider.sendAsync = function() {
-    return HTLContract.currentProvider.send.apply(
-      HTLContract.currentProvider, arguments
+if (typeof HTLC.currentProvider.sendAsync !== "function") {
+  HTLC.currentProvider.sendAsync = function() {
+    return HTLC.currentProvider.send.apply(
+      HTLC.currentProvider, arguments
     );
   };
 }
 
-var HTLERC20Contract = contract(require('../build/contracts/HashedTimelockERC20.json'))
-HTLERC20Contract.setProvider(web3.currentProvider)
+var HTLC_ERC20 = contract(require('../build/contracts/HashedTimelockERC20.json'))
+HTLC_ERC20.setProvider(web3.currentProvider)
+//dirty hack for web3@1.0.0 support for localhost testrpc, see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
+if (typeof HTLC_ERC20.currentProvider.sendAsync !== "function") {
+  HTLC_ERC20.currentProvider.sendAsync = function() {
+    return HTLC_ERC20.currentProvider.send.apply(
+      HTLC_ERC20.currentProvider, arguments
+    );
+  };
+}
 
-var TestTokenContract = contract(require('../build/contracts/TestToken.json'))
-TestTokenContract.setProvider(web3.currentProvider)
+var TESTC = contract(require('../build/contracts/TestToken.json'))
+TESTC.setProvider(web3.currentProvider)
+//dirty hack for web3@1.0.0 support for localhost testrpc, see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
+if (typeof TESTC.currentProvider.sendAsync !== "function") {
+  TESTC.currentProvider.sendAsync = function() {
+    return TESTC.currentProvider.send.apply(
+      TESTC.currentProvider, arguments
+    );
+  };
+}
 
 var accounts = [];
 var part1 = null;
@@ -44,23 +58,50 @@ web3.eth.getAccounts(function(err, accs) {
   part1 = accounts[1];
   part2 = accounts[2];
 
-  initETHAtomicSwap('hello', part2, 2, Date.now() / 1000 + 24 * 60 * 60);
+  initETHAtomicSwap('hello', part2, Date.now() / 1000 + 24 * 60 * 60, 2)
+    .then(args => console.log("args = ", args));
+  //console.log("hashlock = ", hashlock);
 });
 
 
-function initETHAtomicSwap(secret, receiver, sum, timelock) {
+function initETHAtomicSwap(secret, receiver, timelock, sum) {
    let htlc;
-   
-   HTLContract.deployed()
+   let hashlock
+
+   return HTLC.deployed()
     .then(instance => {
       htlc = instance;
       return htlc.hashSecret(secret, {from: part1})
      })
-    .then(hashlock => htlc.newContract(receiver, hashlock, timelock, {from: part1, value: sum, gas: 4000000}))
+    .then(hashedSecret => {
+      hashlock = hashedSecret;
+      return htlc.newContract(receiver, hashlock, timelock, {from: part1, value: sum, gas: 4000000})
+    })
     .then(tx => {
-          const log = tx.logs[0];
+          const log = tx.logs[0]
           console.log("Log event  = "  + log.event);
           console.log("Log args = ", log.args);
+
+          return log.args;
      })
      .catch(err => console.error("error = " + err))
+}
+
+function initERC20AtomicSwap(receiver, hashlock, timelock, tokenContract, sum) {
+  let htlc;
+
+  return HTLC_ERC20.deployed()
+   .then(instance => {
+     htlc = instance;
+     return htlc.newContract(receiver, hashlock, timelock, tokenContract, sum,
+           {from: part2, gas: 4000000})
+    })
+   .then(tx => {
+         const log = tx.logs[0]
+         console.log("Log event  = "  + log.event);
+         console.log("Log args = ", log.args);
+
+         return tx.args.contractId;
+    })
+    .catch(err => console.error("error = " + err))
 }
